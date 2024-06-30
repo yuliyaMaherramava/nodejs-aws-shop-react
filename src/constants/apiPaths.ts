d540da1f9ffed970332570473d72e3a6b0408767
Yuliya <y.magerramova@itransition.com>
2024-06-30 15:51:25 +0200

    Change api paths, uncomment code of csv parser

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
   product: "https://hbeblnv7o0.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://93nnm5h3ui.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
